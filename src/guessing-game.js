e124cf49826523bf6cb7ff3b6104ec44a1b0f142
Maria Goenko <masha@dev.by>
2019-10-20 12:08:19 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.curMin = 0;
+      this.curMax = 0;
+      this.curMid = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
+      this.curMin = this.min;
+      this.curMax = this.max;
     }
 
     guess() {
-
+      return this.recalculateMid();
     }
 
     lower() {
-
+      if(this.curMax > this.curMin) {
+        this.curMax = this.curMid;
+      }
     }
 
     greater() {
+      if(this.curMin < this.curMax) {
+        this.curMin = this.curMid;
+      }
+    }
 
+    recalculateMid() {
+      this.curMid = this.curMin + Math.ceil((this.curMax - this.curMin) / 2);
+      return this.curMid;
     }
 }
 
